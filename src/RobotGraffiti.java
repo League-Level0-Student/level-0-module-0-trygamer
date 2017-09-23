bd975a6ae756d14c1d0ef2b52665ff074873a617
trygamer <league@WorthyMini-5.attlocal.net>
2017-09-23 14:01:08 -0700

    gu kvd¬å∆© ¢†yh

--- src/RobotGraffiti.java
+++ src/RobotGraffiti.java
@@ -3,10 +3,60 @@ import org.jointheleague.graphical.robot.Robot;
 public class RobotGraffiti {
 public static void main(String[] args) {
 Robot K = new Robot();
-K.moveTo(200,400);
+K.moveTo(100,400);
 K.penDown();
 K.setPenColor(0, 0, 0);
 K.setSpeed(10);
-K.move (350);
+K.move (150);
+K.turn(180);
+K.move(75);
+K.turn(230);
+K.move(100);
+K.turn(180);
+K.move(100);
+K.turn(260);
+K.move(100);
+K.turn(320);
+K.penUp();
+K.move(50);
+K.penDown();
+K.turn(300);
+K.move(150);
+K.turn(140);
+K.move(80);
+K.turn(100);
+K.move(60);
+K.turn(180);
+K.move(60);
+K.turn(80);
+K.move(75);
+K.turn(280);
+K.penUp();
+K.move(50);
+K.penDown();
+K.turn(245);
+K.move(80);
+K.turn(50);
+K.move(90);
+K.turn(150);
+K.move(180);
+K.turn(270);
+K.penUp();
+K.move(50);
+K.penDown();
+K.move(80);
+K.turn(180);
+K.move(80);
+K.turn(90);
+K.move(90);
+
+
+
+
+
+
+
+
+
 }
 }
